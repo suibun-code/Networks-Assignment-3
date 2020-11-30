9d28cd7aacbb83b4fcee4967e9c28355faabdba1
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-11-29 23:46:42 -0500

    Assignment3 Gold

--- assignment3_client.py
+++ assignment3_client.py
@@ -8,17 +8,69 @@ import requests
 import json
 import random
 
+UpdateSkillURL = "https://94ghzerne7.execute-api.us-east-1.amazonaws.com/default/UpdateElo"
+UpdateAPI_KEY = "1trpw9xEYPPt38nOp2LrawmtEgjhiPK3lOKVaALi"
+Updateheaders = { "x-api-key": UpdateAPI_KEY}
+
+GetPlayersURL = "https://vfrlx7pdxl.execute-api.us-east-1.amazonaws.com/default/hello-world"
+PlayersAPI_KEY = "tFm7YmutpZ9nZ9NuWi0Hda96PgxdcvJw3ktOx6bz"
+Playersheaders = { "x-api-key": PlayersAPI_KEY}
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ipAddress = "localhost"
 port = 12345
 
-players = '0'
+#To actually simulate the matches, a preset of player IDs need to be
+#stored so we can test this. Otherwise 3 clients would need to be opened.
+#All other IDs other than the one sent to the server are considered already 'matchmaking'.
+players = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def main():
-    #matchCount = input("How many games should the simulation play? ")
-    sock.connect((ipAddress, port))
-    sock.sendto(bytes(players,'utf8'), (ipAddress, port))
+    logging.basicConfig(filename='Assignment3.log', level=logging.INFO)
 
+    i = 0
+    matchCount = input("How many games should the simulation play? ")
+    logging.info('Simulating ' +  matchCount + ' matches.')
+
+    while i < int(matchCount):
+        logging.info('Beginning Game#' + str(i))
+        thisPlayer = random.choice(players)
+        SimulateMatchmaking(thisPlayer)
+        i += 1
+
+def SimulateMatchmaking(thisPlayer):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ipAddress, port))
+        sock.sendto(bytes(thisPlayer, 'utf8'), (ipAddress, port))
+        data = sock.recv(1024)
+
+    matchMakedPlayers = json.loads(data)
+    logging.info('Matched players: ID#' + matchMakedPlayers[0]['player_id'] + ', ID#' + matchMakedPlayers[1]['player_id'] + ', ID#' + matchMakedPlayers[2]['player_id'])
+
+    # for player in matchMakedPlayers:
+    #    print("Player ID: " + player['player_id'])
+    #    print("Player Name: " + str(player['player_name']))
+    #    print("Player Skill: " + str(player['player_skill']))
+    #    print("------------------")
+
+    winningPlayer = random.choice(matchMakedPlayers)
+    matchMakedPlayers.remove(winningPlayer)
+
+    print(winningPlayer)
+    logging.info('Winning player: ID#' + winningPlayer['player_id'])
+
+    #Update the winning player's skill
+    params = {'player_id': winningPlayer['player_id'], 'result': 1}
+    requests.get(UpdateSkillURL, headers=Updateheaders, params=params)
+
+    #Update the lossing player's skills
+    print("Players that lost:")
+    print("------------------")
+    for player in matchMakedPlayers:
+        print("Player Name: " + str(player['player_name']))
+        params = {'player_id': player['player_id'], 'result': 0}
+        requests.get(UpdateSkillURL, headers=Updateheaders, params=params)
+        logging.info('Losing player: ID#' + player['player_id'])
 
 if __name__ == '__main__':
    main()
--- assignment3_server.py
+++ assignment3_server.py
@@ -3,6 +3,7 @@ from _thread import *
 import threading
 from datetime import datetime
 import logging
+import logging.config
 import time
 import socket
 import requests
@@ -12,10 +13,13 @@ URL = "https://vfrlx7pdxl.execute-api.us-east-1.amazonaws.com/default/hello-worl
 API_KEY = "tFm7YmutpZ9nZ9NuWi0Hda96PgxdcvJw3ktOx6bz"  # API key
 headers = { "x-api-key": API_KEY }
 
+skillThreshold = 650
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 12345
 
 def main():
+    logging.basicConfig(filename='Assignment3Server.log', level=logging.INFO)
     sock.bind(('', port))
     start_new_thread(MakeApiRequestToGetPlayers, (sock,))
     while True:
@@ -25,33 +29,56 @@ def main():
 
 def MakeApiRequestToGetPlayers(sock):
     while True:
+        #Listen for a connection from a client.
         sock.listen()
+
+        #Data recieved and from where stored in variables.
         data, addr = sock.accept()
 
-        IDofPlayer = data.recv(1024)
-        IDofPlayer = IDofPlayer.decode("utf-8")
-        print(IDofPlayer)
+        #The data (ID of player in this case) stored as a string.
+        RequesterID = data.recv(1024)
+        RequesterID = RequesterID.decode("utf-8")
+        print("Player ID #" + RequesterID + " looking for a match.")
+        logging.info('The player ID#' + RequesterID + ' has connected.')
 
+        #Get all the items from the player_id database
         response = requests.get(URL, headers=headers)
         body = json.loads(response.content)
         players = body.get('Items')
 
-        selectedPlayers = sorted(players, key=lambda elem: elem['player_skill'])
-
-
-
-        #Debugging
-        #for player in players:
-        #    print("Player ID: " + player['player_id'])
-        #    print("Player Name: " + player['player_name'])
-        #    print("Player Skill: " + player['player_skill'])
-        #    print("------------------")
+        response = MakeMatch(players, RequesterID)
+        data.send(bytes(response, 'utf8'))
+        
 
 def ActiveIndicator(sock):
     print("Server listening...")
 
-def MakeMatch():
-    print("hi")
+def MakeMatch(players, RequesterID):
+    #Get the player information of the player requesting the match
+    for player in players:
+        if player['player_id'] == RequesterID:
+            RequestingPlayer = player
+
+    #Attempt to find players suitable to play with this player.
+
+    playersByAscendingSkillLevel = sorted(players, key=lambda elem: elem['player_skill'])
+
+    matchMakedPlayers = []
+    matchMakedPlayers.append(RequestingPlayer)
+
+    for player in players:
+        if player != RequestingPlayer and abs(player['player_skill'] - RequestingPlayer['player_skill']) < skillThreshold:
+            if len(matchMakedPlayers) < 3:
+                matchMakedPlayers.append(player)
+
+    #for player in matchMakedPlayers:
+    #    print("Player ID: " + player['player_id'])
+    #    print("Player Name: " + str(player['player_name']))
+    #    print("Player Skill: " + str(player['player_skill']))
+    #    print("------------------")
+
+    response = json.dumps(matchMakedPlayers)
+    return response
 
 if __name__ == '__main__':
    main()
